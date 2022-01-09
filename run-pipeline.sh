6bd94432555692c04683227809fcc66389672768
Fraser Crichton <52700324+frasercrichton@users.noreply.github.com>
2022-01-09 14:57:12 +1300

    Dependabot

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Run Kettling pipeline"
-# aws codepipeline start-pipeline-execution --name frasercrichton-com-kettling
+aws codepipeline start-pipeline-execution --name frasercrichton-com-kettling
 aws cloudfront create-invalidation --distribution-id EYLFA9OG0T7L9 --paths "/*" 
 
 # aws codepipeline get-pipeline-state --name frasercrichton-com-kettling
